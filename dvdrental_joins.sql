b9eadbe998582331ae0ba78d30332fc7c83b63df
Crystal Sanders <crystalsanders@Crystals-MacBook-Pro.local>
2020-10-21 19:31:54 -0500

    first commit

---
+++ dvdrental_joins.sql
@@ -0,0 +1,75 @@
+-- 1. List all customers who live in Texas (use
+-- JOINs)
+SELECT first_name,last_name,district
+FROM address
+JOIN customer
+ON address.address_id = customer.address_id
+WHERE district = 'Texas';
+
+--2. Get all payments above $6.99 with the Customer's Full
+--Name
+SELECT first_name,last_name,amount
+FROM customer
+JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE amount > 6.99
+ORDER BY amount ASC;
+
+
+--3. Show all customers names who have made payments over $175(use
+--subqueries)
+SELECT store_id,first_name,last_name
+FROM customer
+WHERE customer_id IN (
+SELECT customer_id
+FROM payment
+GROUP BY customer_id
+HAVING SUM(amount) > 175
+ORDER BY SUM(amount) DESC
+);
+
+
+--4. List all customers that live in Nepal (use the city
+--table)
+SELECT first_name,last_name,city
+FROM customer
+FULL JOIN address
+ON customer.address_id = address.address_id
+FULL JOIN city
+ON address.city_id = city.city_id
+WHERE city = 'Nepal';
+
+--5. Which staff member had the most
+--transactions?
+SELECT COUNT(last_name),last_name,first_name
+FROM rental
+FULL JOIN staff
+ON rental.staff_id = staff.staff_id
+GROUP BY last_name,first_name
+ORDER BY last_name;
+
+--6. How many movies of each rating are
+--there?
+SELECT COUNT(rating),rating
+FROM film
+GROUP BY rating
+ORDER BY rating;
+
+--7.Show all customers who have made a single payment
+--above $6.99 (Use Subqueries)
+SELECT store_id,first_name,last_name
+FROM customer
+WHERE customer_id IN (
+SELECT customer_id
+FROM payment
+GROUP BY customer_id,amount
+HAVING amount > 6.99 AND COUNT(payment_id) = 1	
+ORDER BY amount DESC
+);
+
+--8. How many free rentals did our stores give away?
+SELECT COUNT(customer.customer_id)
+FROM customer
+JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE amount = 0;
